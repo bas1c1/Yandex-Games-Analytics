c1015eefc52e318da0d5af6b942815f0add13607
bas1c <68533770+bas1c1@users.noreply.github.com>
2024-06-19 15:10:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-docker buildx build -t yandex_game_analysis . --load
-docker run -d -p 5000:80 yandex_game_analysis
+#Dockerfile нуждается в доработке, так как Selenium не запускается в Docker.
+
+#docker buildx build -t yandex_game_analysis . --load
+#docker run -d -p 5000:80 yandex_game_analysis
+
+python3 main.py
